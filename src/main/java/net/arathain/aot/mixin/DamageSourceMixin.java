896209b3f7d5b22d74865564d9d1273f5837aafc
Arathain <76701473+Arathain@users.noreply.github.com>
2021-04-26 12:56:38 +0200

    Fix spectral arrow crash

--- src/main/java/net/arathain/aot/mixin/DamageSourceMixin.java
+++ src/main/java/net/arathain/aot/mixin/DamageSourceMixin.java
@@ -2,7 +2,6 @@ package net.arathain.aot.mixin;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.projectile.ArrowEntity;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,7 +15,8 @@ public  class DamageSourceMixin {
     @Inject(method = "arrow", at = @At(value = "RETURN"), cancellable = true)
     private static void changeCrossbowPiercingSource( PersistentProjectileEntity projectile, Entity attacker, CallbackInfoReturnable<DamageSource> cir){
         DamageSource source = cir.getReturnValue() ;
-        ArrowEntity arrow = ((ArrowEntity) source.getSource());
+        PersistentProjectileEntity arrow = ((PersistentProjectileEntity) source.getSource());
+
         if(arrow != null && arrow.isShotFromCrossbow() && arrow.getPierceLevel() > 0){
             DamageSourceAccessor accessor = (DamageSourceAccessor) source;
             accessor.setBypassesArmor(true);
